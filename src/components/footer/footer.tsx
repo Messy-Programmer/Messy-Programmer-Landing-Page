fcf7b4543749cd2f3e2981b1228b1f0ef8702ae1
Deeplobo <77326151+Deeplobo@users.noreply.github.com>
2024-04-05 05:00:35 +0000

    desktop footer and navbar logo added

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -11,44 +11,52 @@ import { FaBuilding } from "react-icons/fa";
 
 const Footer = () => (
   <div className="bg-[#051405] py-14">
-    <div className="w-full dark:bg-black dark:bg-grid-white/[0.2] bg-grid-black relative flex items-center justify-center bg-[#0d260d]">
+    <div className="w-full dark:bg-black dark:bg-grid-white/[0.2] bg-grid-black relative flex items-center justify-center bg-[#0d260d] md:justify-around">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-gradient-to-r from-[#051505] from-1% via-[rgba(74,114,74,0)] via-800% to-[#051505] to-99%"></div>
       <div>
         {/* this is main footer context container */}
-        <div className="flex text-center px-3 flex-col mt-12 mb-14">
-          <h2 className="text-slate-100 z-10 text-2xl">
-            Start Delivering better customer outcomes today
-          </h2>
-          <StaticImage
-            src="../images/messyProgrammerLogo.pmg"
-            alt="messyProgrammerLogo"
-            className=" text-slate-100 text-xl z-10 mt-8"
-          />
-          {/* icon section  */}
-          <div className="flex justify-center gap-2 mt-8">
-            <Link to="#" className="text-white text-xl">
-              <TiSocialLinkedin />
-            </Link>
-            <Link to="#" className="text-white text-xl">
-              <TiSocialInstagram />
-            </Link>
-            <Link to="#" className="text-white text-xl">
-              <TiSocialFacebookCircular />
-            </Link>
-            <Link to="#" className="text-white text-xl">
-              <TiSocialYoutube />
-            </Link>
+        <div className="flex text-center px-3 flex-col mt-12 mb-14 items-center md:flex-row md:text-left md:items-start md:justify-between md:gap-8 md:pr-0">
+          <div className="z-10 md:flex-[.8] ">
+            <h2 className="text-slate-100 text-xl">
+              Start Delivering better customer outcomes today
+            </h2>
+            <StaticImage
+              src="../../images/messyProgrammerLogo.png"
+              alt="messyProgrammerLogo"
+              className="z-10 mt-8 w-48 md:mt-[30px]"
+            />
+            {/* icon section  */}
+            <div className="flex justify-center gap-2 mt-8 md:mt-[30px] md:justify-start">
+              <Link to="#" className="text-white text-xl">
+                <TiSocialLinkedin />
+              </Link>
+              <Link to="#" className="text-white text-xl">
+                <TiSocialInstagram />
+              </Link>
+              <Link to="#" className="text-white text-xl">
+                <TiSocialFacebookCircular />
+              </Link>
+              <Link to="#" className="text-white text-xl">
+                <TiSocialYoutube />
+              </Link>
+            </div>
           </div>
           {/* this is link section  */}
           <div>
-            <p className="text-slate-100 mt-16">Portfolio</p>
-            <p className="text-slate-100 mt-[18px]">Privacy Policy </p>
-            <p className="text-slate-100 mt-[18px]">Terms & Conditions</p>
-            <p className="text-slate-100 mt-[18px]">Copyright Notice</p>
+            <p className="text-slate-100 mt-16 md:mt-0">Portfolio</p>
+            <p className="text-slate-100 mt-[18px] md:mt-[25px]">
+              Privacy Policy{" "}
+            </p>
+            <p className="text-slate-100 mt-[18px] md:mt-[25px]">
+              Terms & Conditions
+            </p>
+            <p className="text-slate-100 mt-[18px] md:mt-[25px]">
+              Copyright Notice
+            </p>
           </div>
           {/* this is the contact us section  */}
-          <div className="relative mt-16">
+          <div className="relative mt-16 md:mt-0">
             <p className="text-[#1E951E] text-sm font-normal z-10">
               CONTACT US
             </p>
@@ -63,9 +71,9 @@ const Footer = () => (
                 +91 86176 38628
               </Link>
             </div>
-            <div className="flex justify-center px-9 mt-5">
+            <div className="flex justify-center px-9 mt-5 md:px-0 md:gap-1 ">
               <FaBuilding className="text-slate-100" />
-              <p className="text-slate-100 text-xs">
+              <p className="text-slate-100 text-xs md:w-[14rem]">
                 Vaoal More, Koch Bihar, West Bengal, India
               </p>
             </div>
--- src/components/navBar/navbar.tsx
+++ src/components/navBar/navbar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const NavBar = () => {
   return (
     <nav className=" absolute top-0 w-full bg-gradient-to-b from-[#061906] from-2% via-[#06190642]">
       <div className="flex justify-between items-center px-2 py-2">
         <div className="log">
-          <h1 className=" text-slate-100 font-medium it">Messy programmer</h1>
+          <StaticImage
+            src="../../images/messyProgrammerLogo.png"
+            alt="messyProgrammerLogo"
+            className=" text-slate-100 font-medium it w-48"
+          />
         </div>
         <Link
           to="/"
