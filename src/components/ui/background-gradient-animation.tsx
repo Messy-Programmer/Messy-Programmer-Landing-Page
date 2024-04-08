0c316b772b5aa9a75464ae7936acfb00771e5700
Deeplobo <77326151+Deeplobo@users.noreply.github.com>
2024-04-08 12:02:56 +0000

    second container added

--- src/components/ui/background-gradient-animation.tsx
+++ src/components/ui/background-gradient-animation.tsx
@@ -10,8 +10,8 @@ export const BackgroundGradientAnimation = ({
   thirdColor = "50,116,50",
   fourthColor = "50,116,50",
   fifthColor = "50,116,50",
-  pointerColor = "50,116,50",
-  size = "65%",
+  pointerColor = "",
+  size = "80%",
   blendingValue = "hard-light",
   children,
   className,
@@ -88,100 +88,96 @@ export const BackgroundGradientAnimation = ({
 
   return (
     <div className="bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]">
-      <div className="logoBg bg-[length:112px_128px] bg-[30%_10%]">
-        <div className="logoBg bg-[length:112px_128px] bg-[10%_90%]">
+      <div
+        className={cn(
+          "h-screen w-full relative overflow-hidden top-0 left-0 md:h-screen",
+          containerClassName
+        )}
+      >
+        <svg className="hidden">
+          <defs>
+            <filter id="blurMe">
+              <feGaussianBlur
+                in="SourceGraphic"
+                stdDeviation="10"
+                result="blur"
+              />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+
+        <div
+          className={cn(
+            "gradients-container h-full w-full blur-lg",
+            isSafari ? "blur-2xl" : "[filter:url(#blurMe)_blur(30px)]"
+          )}
+        >
           <div
             className={cn(
-              "h-screen w-full relative overflow-hidden top-0 left-0 md:h-screen",
-              containerClassName
+              `absolute [background:radial-gradient(circle_at_center,_var(--first-color)_0,_var(--first-color)_50%)_no-repeat]`,
+              `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+              `[transform-origin:center_center]`,
+              `animate-first`,
+              `opacity-80 md:opacity-85`
             )}
-          >
-            <svg className="hidden">
-              <defs>
-                <filter id="blurMe">
-                  <feGaussianBlur
-                    in="SourceGraphic"
-                    stdDeviation="10"
-                    result="blur"
-                  />
-                  <feColorMatrix
-                    in="blur"
-                    mode="matrix"
-                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                    result="goo"
-                  />
-                  <feBlend in="SourceGraphic" in2="goo" />
-                </filter>
-              </defs>
-            </svg>
+          ></div>
+          <div
+            className={cn(
+              `absolute [background:radial-gradient(circle_at_center,_rgba(var(--second-color),_0.8)_0,_rgba(var(--second-color),_0)_50%)_no-repeat]`,
+              `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+              `[transform-origin:calc(50%-400px)]`,
+              `animate-second`,
+              `opacity-80`
+            )}
+          ></div>
+          <div
+            className={cn(
+              `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.8)_0,_rgba(var(--third-color),_0)_50%)_no-repeat]`,
+              `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+              `[transform-origin:calc(50%+400px)]`,
+              `animate-third`,
+              `opacity-70`
+            )}
+          ></div>
+          <div
+            className={cn(
+              `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fourth-color),_0.8)_0,_rgba(var(--fourth-color),_0)_50%)_no-repeat]`,
+              `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+              `[transform-origin:calc(50%-200px)]`,
+              `animate-fourth`,
+              `opacity-70`
+            )}
+          ></div>
+          <div
+            className={cn(
+              `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_50%)_no-repeat]`,
+              `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+              `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,
+              `animate-fifth`,
+              `opacity-70`
+            )}
+          ></div>
 
+          {interactive && (
             <div
+              ref={interactiveRef}
+              onMouseMove={handleMouseMove}
               className={cn(
-                "gradients-container h-full w-full blur-lg",
-                isSafari ? "blur-2xl" : "[filter:url(#blurMe)_blur(30px)]"
+                `absolute [background:radial-gradient(circle_at_center,_rgba(var(--pointer-color),_0.8)_0,_rgba(var(--pointer-color),_0)_50%)_no-repeat]`,
+                `[mix-blend-mode:var(--blending-value)] w-full h-full -top-1/2 -left-1/2`,
+                `opacity-60`
               )}
-            >
-              <div
-                className={cn(
-                  `absolute [background:radial-gradient(circle_at_center,_var(--first-color)_0,_var(--first-color)_50%)_no-repeat]`,
-                  `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
-                  `[transform-origin:center_center]`,
-                  `animate-first`,
-                  `opacity-60 md:opacity-85`
-                )}
-              ></div>
-              <div
-                className={cn(
-                  `absolute [background:radial-gradient(circle_at_center,_rgba(var(--second-color),_0.8)_0,_rgba(var(--second-color),_0)_50%)_no-repeat]`,
-                  `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
-                  `[transform-origin:calc(50%-400px)]`,
-                  `animate-second`,
-                  `opacity-60`
-                )}
-              ></div>
-              <div
-                className={cn(
-                  `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.8)_0,_rgba(var(--third-color),_0)_50%)_no-repeat]`,
-                  `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
-                  `[transform-origin:calc(50%+400px)]`,
-                  `animate-third`,
-                  `opacity-70`
-                )}
-              ></div>
-              <div
-                className={cn(
-                  `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fourth-color),_0.8)_0,_rgba(var(--fourth-color),_0)_50%)_no-repeat]`,
-                  `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
-                  `[transform-origin:calc(50%-200px)]`,
-                  `animate-fourth`,
-                  `opacity-60`
-                )}
-              ></div>
-              <div
-                className={cn(
-                  `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_50%)_no-repeat]`,
-                  `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
-                  `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,
-                  `animate-fifth`,
-                  `opacity-70`
-                )}
-              ></div>
-
-              {interactive && (
-                <div
-                  ref={interactiveRef}
-                  onMouseMove={handleMouseMove}
-                  className={cn(
-                    `absolute [background:radial-gradient(circle_at_center,_rgba(var(--pointer-color),_0.8)_0,_rgba(var(--pointer-color),_0)_50%)_no-repeat]`,
-                    `[mix-blend-mode:var(--blending-value)] w-full h-full -top-1/2 -left-1/2`,
-                    `opacity-60`
-                  )}
-                ></div>
-              )}
-            </div>
-            <div className={cn("", className)}>{children}</div>
-          </div>
+            ></div>
+          )}
         </div>
+        <div className={cn("", className)}>{children}</div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,79 +1,34 @@
 import * as React from "react";
-import { Link, type HeadFC, type PageProps } from "gatsby";
+import { type HeadFC, type PageProps } from "gatsby";
 import NavBar from "../components/navBar/navbar";
 import Footer from "../components/footer/footer";
 import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation";
-import { IoMail } from "react-icons/io5";
+
+import HeroText from "../components/otherComp/heroText";
+import { BackgroundBeams } from "../components/ui/background-beam";
 import { StaticImage } from "gatsby-plugin-image";
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <>
       <NavBar></NavBar>
       <BackgroundGradientAnimation>
-        <div className="z-50 mt-24 absolute top-0 sm:mt-[10rem] w-full">
-          <div className="flex flex-col max-w-[904px] text-center sm:text-start sm:pl-7 sm:max-w-[45rem] lg:max-w-[50rem]">
-            <div className="w-full text-[1.7rem] font-medium max-md:max-w-full gradient-txt sm:text-4xl lg:text-5xl lg:leading-loose">
-              Your Success Story,
-            </div>
-            <div className="flex gap-5 flex-col px-5 mt-3 sm:px-0 sm:flex-row">
-              <div className="flex gap-2 items-center justify-center px-5 py-2.5 my-auto text-sm font-semibold text-green-400 rounded-3xl bg-zinc-900">
-                <IoMail className="text-lg" />
-                <div className="my-auto">contact@messyprogrammer.in</div>
-              </div>
-              <div className="text-[1.7rem] font-medium max-md:max-w-full gradient-txt sm:text-4xl lg:text-5xl">
-                Our Tech Expertise
-              </div>
-            </div>
-            <div className="mt-3.5 w-full text-sm px-4 leading-6 md:leading-9 text-white max-md:max-w-full sm:px-0">
-              Messy Programmer is your trusted partner for top-notch IT services
-              at affordable prices. With a focus on quality and affordability,
-              we specialize in providing a wide range of IT solutions tailored
-              to meet your business needs.
-            </div>
-            <div className="text-slate-100 mt-5 flex gap-1.5 justify-center sm:justify-start">
-              <Link
-                to="/"
-                className="rounded-2xl max-md:px-5 bg-gradient-to-tr from-green-700 to-lime-500 py-2 px-3"
-              >
-                Contact Us
-              </Link>
-              <Link
-                to=""
-                className="rounded-2xl bg-neutral-800/65 py-2 px-3 max-md:px-5"
-              >
-                Explore Services
-              </Link>
-            </div>
-          </div>
-          {/* hero text end */}
-          {/* tab images for mobile  */}
-          <div className="mt-[2rem] lg:hidden">
-            <div className="flex justify-around pt-6 sm:justify-center sm:gap-3 w-full">
-              <StaticImage
-                className="w-[6rem] h-[12rem]  sm:w-[10rem] sm:h-[20rem]"
-                src="../images/tab-img1.png"
-                alt="lorem"
-              />
-              <StaticImage
-                className="w-[8rem] h-[12rem] mt-12 sm:w-[12rem] sm:h-[16rem] sm:mt-0"
-                src="../images/tab-img2.png"
-                alt=""
-              />
-              <StaticImage
-                className=" w-[6rem] h-[12rem] mt-24 sm:mt-0 sm:w-[10rem] sm:h-[20rem]"
-                src="../images/tab-img1.png"
-                alt=""
-              />
-            </div>
-          </div>
-          {/* tab images for mobile end */}
-        </div>
-        {/* hero image for tab n desktop */}
-        <div className="hidden absolute sm:inline-block sm:top-[20%] sm:right-0 sm:w-[8rem] sm:z-10 lg:right-[10rem] lg:w-[11rem]">
-          <StaticImage src="../images/LogoVector.png" alt="logo" />
-        </div>
-        {/* hero image for tab n desktop end */}
+        <HeroText />
       </BackgroundGradientAnimation>
+      <div className="relative hidden md:inline-block bg-gradient-to-b from-[#0d270d] to-[#051405] mix-blend-multiply">
+        <div className="flex py-36 px-20 w-[full] lg:px-24 lg:py-40">
+          <div>
+            <StaticImage src="../images/projectConsole.png" alt="" />
+          </div>
+          <div>
+            <StaticImage src="../images/projectDev.png" alt="" />
+          </div>
+          <div className="flex flex-col">
+            <StaticImage src="../images/projectCode.png" alt="" />
+            <StaticImage src="../images/projectTerminal.png" alt="" />
+          </div>
+        </div>
+        <BackgroundBeams />
+      </div>
       <Footer />
     </>
   );
