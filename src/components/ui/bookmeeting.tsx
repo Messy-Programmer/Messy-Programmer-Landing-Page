6f812c0ea351252e76ed0ebe92cc2d9a7632e06e
Deeplobo <77326151+Deeplobo@users.noreply.github.com>
2024-06-27 10:49:31 +0000

    contact us right side text mb changed for mobile version

--- src/components/ui/bookmeeting.tsx
+++ src/components/ui/bookmeeting.tsx
@@ -5,14 +5,14 @@ const BookMeeting = () => {
   return (
     <div className="bg-grid-black border-[#1b321b] border-2 rounded-xl relative z-5 lg:py-7 xl:py-6">
       <div className="flex gap-3 h-full flex-col  text-start leading-5 lg:gap-0 lg:px-8 z-10 relative text-slate-100 p-8">
-        <p className="gradient-txt lg:mb-10 xl:mb-14">BOOK YOUR FIRST</p>
+        <p className="gradient-txt mb-7 lg:mb-10 xl:mb-14">BOOK YOUR FIRST</p>
         <h2 className="gradient-txt font-medium text-4xl lg:font-extrabold lg:text-6xl">
           Appointment
         </h2>
-        <h2 className="text-4xl lg:font-extrabold lg:mb-14 lg:text-6xl">
+        <h2 className="text-4xl mb-7 lg:font-extrabold lg:mb-14 lg:text-6xl">
           Free
         </h2>
-        <p className="text-[.7rem] gradient-txt text-center lg:text-xs lg:font-semibold lg:mb-10 xl:mb-14">
+        <p className=" mb-3 text-[.7rem] gradient-txt text-center lg:text-xs lg:font-semibold lg:mb-10 xl:mb-14">
           You will receive a confirmation message after submitting the form, and
           our executive will get back to you within a day to confirm the
           meeting.
