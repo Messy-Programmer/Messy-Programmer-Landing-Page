bf13930d48213fdd8aa5f794b306c2d0b81e6a97
Deeplobo <77326151+Deeplobo@users.noreply.github.com>
2024-06-27 11:02:43 +0000

    lets talk button glitch fixed

--- src/components/ui/hover-border-gradient.tsx
+++ src/components/ui/hover-border-gradient.tsx
@@ -61,7 +61,7 @@ export function HoverBorderGradient({
       }}
       onMouseLeave={() => setHovered(false)}
       className={cn(
-        "relative flex rounded-full content-center bg-[#051405] hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
+        "relative flex rounded-full content-center hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
         containerClassName
       )}
       {...props}
