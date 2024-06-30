84b5d9f61da6268e20f713e3e1cf7f231c44f1fd
Deeplobo <77326151+Deeplobo@users.noreply.github.com>
2024-06-30 09:54:20 +0000

    has added in set timeout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,13 +22,16 @@ const IndexPage: React.FC<PageProps> = () => {
   const delay = 5;
   let hash = window.location.hash;
   let timer1: NodeJS.Timeout;
+  let timer2: NodeJS.Timeout;
   const [IsLoading, SetLoading] = useState(true);
   useEffect(() => {
     timer1 = setTimeout(() => SetLoading(false), delay * 1000);
-    // console.log(hash);
-    $("html, body").animate({ scrollTop: $(hash)?.offset()?.top });
+    timer2 = setTimeout(() => {
+      $("html, body").animate({ scrollTop: $(hash)?.offset()?.top });
+    }, (delay + 1) * 1000);
     return () => {
       clearTimeout(timer1);
+      clearTimeout(timer2);
     };
   }, [hash]);
 
