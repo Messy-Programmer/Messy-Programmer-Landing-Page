b7af325a4e3e681445d3736e59e87b5b240a2b6b
Deeplobo <77326151+Deeplobo@users.noreply.github.com>
2024-07-27 09:16:37 +0000

    font changed from xtra light to light

--- src/components/otherComp/AppointMent.tsx
+++ src/components/otherComp/AppointMent.tsx
@@ -8,7 +8,7 @@ const AppointMent = () => {
           <h1 className="gradient-txt font-semibold text-5xl mb-4 sm:leading-[3.5rem] z-40">
             Every Idea is different
           </h1>
-          <p className="font-extralight z-40">
+          <p className="font-light z-40">
             Join us for a free session to discuss your project ideas and
             requirements. Share your vision, receive expert feedback, and get a
             clear roadmap to bring your ideas to life
@@ -17,11 +17,11 @@ const AppointMent = () => {
       </div>
       <div id="grid-box" className="px-2 sm:px-4">
         <div className="text-slate-100 px-2 py-6 flex flex-col items-center text-center rounded-2xl border-2 border-t-green-900/55 border-l-green-950/55 border-r-green-950/35 border-b-green-950/35 sm:py-10 lg:px-28 xl:px-60 bg-grid-black relative z-0 overflow-hidden">
-          <p className="font-extralight z-40">BOOK YOUR FIRST</p>
+          <p className="font-light z-40">BOOK YOUR FIRST</p>
           <h1 className="text-5xl gradient-txt font-semibold z-40">
             Appointment Free
           </h1>
-          <p className="font-extralight py-8 z-40">
+          <p className="font-light py-8 z-40">
             You will receive a confirmation mail after booking an appointment.
             Once confirmed, you can join using the shared meeting link on the
             selected date and time.
--- src/pages/appointment-booked.tsx
+++ src/pages/appointment-booked.tsx
@@ -30,90 +30,95 @@ const SubmitPage = () => {
   });
 
   return (
-    <div className="h-screen relative w-full bg-[#051405] flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <div className="w-full absolute inset-0 h-full">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.7}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#39AC39"
-        />
-      </div>
-      <div
-        id="container-SubmitPage"
-        className="submit-bg py-10 2xl:flex 2xl:justify-center 2xl:items-center xl:py-20 3xl:py-[50rem] 3xl:w-full sm:h-full sm:w-full"
-      >
-        <div id="inner-container" className="text-slate-100 xl:px-60 lg:px-32">
-          <div id="gif">
-            <div className="flex justify-center">
-              <img
-                src={submitGif}
-                alt="gif"
-                className="size-24 p-4 bg-green-700/20 rounded-full"
-              />
-            </div>
-          </div>
-          <div id="heading-submit" className="text-center px-2 mb-8">
-            <h1 className="gradient-txt text-4xl font-semibold mb-4">
-              Your booking has been submitted
-            </h1>
-            <p className="font-extralight text-sm">
-              Messy Programmer still needs to confirm or reject the bookings.
-            </p>
-          </div>
+    <>
+      <div className="h-screen relative w-full bg-[#051405] flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <div className="w-full absolute inset-0 h-full">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.7}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#39AC39"
+          />
+        </div>
+        <div
+          id="container-SubmitPage"
+          className="submit-bg py-10 2xl:flex 2xl:justify-center 2xl:items-center xl:py-20 3xl:py-[50rem] 3xl:w-full sm:h-full sm:w-full"
+        >
           <div
-            id="grid-box"
-            className="border-2 rounded-2xl border-t-[#2E4F2E] border-l-[#2E4F2E] border-b-[#112311] border-r-[#112311] px-4 py-4 gap-4 mx-2 mb-8 relative z-0 bg-grid-black overflow-hidden sm:mx-14 xl:mx-20"
+            id="inner-container"
+            className="text-slate-100 xl:px-60 lg:px-32"
           >
-            <div className="submit-bg1 z-20 h-full w-full absolute inset-0"></div>
-            <div className="submit-bg2 z-10 h-full w-full absolute inset-0"></div>
-            <table className="table-auto z-40 relative border-separate border-spacing-x-2 border-spacing-y-2  sm:mx-auto sm:border-spacing-x-8 sm:border-spacing-y-4 lg:border-spacing-x-6 lg:border-spacing-y-8 xl:border-spacing-x-16">
-              <tbody>
-                <tr>
-                  <td className="align-top font-semibold">
-                    <HiMiniQuestionMarkCircle className="size-5 inline mr-2" />
-                    What
-                  </td>
-                  <td className="font-extralight">
-                    Discovery meeting between Messy Programmer and Suman Das
-                  </td>
-                </tr>
-                <tr>
-                  <td className="align-top flex font-semibold">
-                    <IoCalendar className="size-5 inline mr-2" />
-                    When
-                  </td>
-                  <td className="font-extralight">
-                    Starts -{" "}
-                    <span className="text-lime-500"> {istStartTime}</span> End-{" "}
-                    <span className="text-red-400"> {istEndTime}</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="align-top flex font-semibold">
-                    <FaLocationDot className="size-5 inline mr-2" />
-                    Where
-                  </td>
-                  <td className="font-extralight">
-                    Google meet link will be sent to your confirmation email-{" "}
-                    <span className="text-lime-500">{email}</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="flex font-semibold"></td>
-                  <td className="font-extralight text-right text-lime-500 hover:text-lime-300 transition duration-300">
-                    <a href="/">← Back to Home</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div id="gif">
+              <div className="flex justify-center">
+                <img
+                  src={submitGif}
+                  alt="gif"
+                  className="size-24 p-4 bg-green-700/20 rounded-full"
+                />
+              </div>
+            </div>
+            <div id="heading-submit" className="text-center px-2 mb-8">
+              <h1 className="gradient-txt text-4xl font-semibold mb-4">
+                Your booking has been submitted
+              </h1>
+              <p className="font-extralight text-sm">
+                Messy Programmer still needs to confirm or reject the bookings.
+              </p>
+            </div>
+            <div
+              id="grid-box"
+              className="border-2 rounded-2xl border-t-[#2E4F2E] border-l-[#2E4F2E] border-b-[#112311] border-r-[#112311] px-4 py-4 gap-4 mx-2 mb-8 relative z-0 bg-grid-black overflow-hidden sm:mx-14 xl:mx-20"
+            >
+              <div className="submit-bg1 z-20 h-full w-full absolute inset-0"></div>
+              <div className="submit-bg2 z-10 h-full w-full absolute inset-0"></div>
+              <table className="table-auto z-40 relative border-separate border-spacing-x-2 border-spacing-y-2  sm:mx-auto sm:border-spacing-x-8 sm:border-spacing-y-4 lg:border-spacing-x-6 lg:border-spacing-y-8 xl:border-spacing-x-16">
+                <tbody>
+                  <tr>
+                    <td className="align-top font-semibold">
+                      <HiMiniQuestionMarkCircle className="size-5 inline mr-2" />
+                      What
+                    </td>
+                    <td className="font-extralight">
+                      Discovery meeting between Messy Programmer and Suman Das
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="align-top flex font-semibold">
+                      <IoCalendar className="size-5 inline mr-2" />
+                      When
+                    </td>
+                    <td className="font-extralight">
+                      Starts -{" "}
+                      <span className="text-lime-500"> {istStartTime}</span>{" "}
+                      End- <span className="text-red-400"> {istEndTime}</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="align-top flex font-semibold">
+                      <FaLocationDot className="size-5 inline mr-2" />
+                      Where
+                    </td>
+                    <td className="font-extralight">
+                      Google meet link will be sent to your confirmation email-{" "}
+                      <span className="text-lime-500">{email}</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="flex font-semibold"></td>
+                    <td className="font-extralight text-right text-lime-500 hover:text-lime-300 transition duration-300">
+                      <a href="/">← Back to Home</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default SubmitPage;
